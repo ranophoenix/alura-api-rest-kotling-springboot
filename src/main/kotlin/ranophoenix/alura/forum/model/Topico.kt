25b8755bb38bc2559f314fb1162dedf9e74c59bd
Robert Anderson <robert.oliveira@tjse.jus.br>
2021-06-18 10:48:15 -0300

    Mapeamento das entidades.

--- src/main/kotlin/ranophoenix/alura/forum/model/Topico.kt
+++ src/main/kotlin/ranophoenix/alura/forum/model/Topico.kt
@@ -1,14 +1,21 @@
 package ranophoenix.alura.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Topico(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val titulo: String,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val curso: Curso,
+    @ManyToOne
     val autor: Usuario,
+    @Enumerated(value = EnumType.STRING)
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    @OneToMany(mappedBy = "topico")
     val respostas: List<Resposta> = ArrayList()
 )
