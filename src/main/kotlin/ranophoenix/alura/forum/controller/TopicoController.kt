d63b3c43563bb6b5674753d7e4027a2a35a66dd0
Robert Anderson <robert.oliveira@tjse.jus.br>
2021-06-25 10:14:42 -0300

    Ordenação.

--- src/main/kotlin/ranophoenix/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/ranophoenix/alura/forum/controller/TopicoController.kt
@@ -2,6 +2,7 @@ package ranophoenix.alura.forum.controller
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -21,7 +22,7 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
-        @PageableDefault(size = 5) paginacao: Pageable
+        @PageableDefault(size = 5, sort = ["data_criacao"], direction = Sort.Direction.DESC) paginacao: Pageable
     ): Page<TopicoView> {
         return service.listar(nomeCurso, paginacao)
     }
